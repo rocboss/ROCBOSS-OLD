0ca51c611ec38944aa2e6296cc8cd57ffb2caa2d
ROC <i@rocs.me>
2017-03-14 20:38:54 +0800

    version

--- app/views/_footer.php
+++ app/views/_footer.php
@@ -3,7 +3,7 @@
 </div>
 <footer class="main-footer">
   <div class="pull-right hidden-xs">
-    浙ICP备15008828号-2. 公安部备案 32010502010016. <b>Version</b> 2.2.1 Beta
+    浙ICP备15008828号-2. 公安部备案 32010502010016. <b>Version</b> 2.2.1
   </div>
   <strong>Copyright &copy; 2016 <a href="https://www.rocboss.com" target="_blank">ROCBOSS</a>.</strong> All rights
   reserved.
